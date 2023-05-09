5dd5be7661e291a083ee9f330d49b09256af254a
William Wong <compulim@hotmail.com>
2023-05-09 01:28:57 -0700

    Better message

--- packages/code-snippet/src/index.ts
+++ packages/code-snippet/src/index.ts
@@ -1,7 +1,13 @@
 console.log('Snippet loaded.');
 
 (async function () {
-  const res = await fetch('http://localhost:8002/api/origin', { method: 'POST' });
+  const url = document.currentScript?.getAttribute('data-url');
+
+  if (!url) {
+    throw new Error('"data-url" attribute must be present on the <script> tag.');
+  }
+
+  const res = await fetch(url, { method: 'POST' });
 
   if (!res.ok) {
     return console.log(`Service returned error ${res.status}`);
@@ -10,4 +16,16 @@ console.log('Snippet loaded.');
   const json = await res.json();
 
   console.log(`Service returned object`, json);
+
+  if (json.origin === location.origin) {
+    console.log(
+      '%c✅ This is correct behavior. The service should return http://localhost:8000, which is the origin of the HTML file that ping http://localhost:8002.',
+      'font-family: sans-serif;'
+    );
+  } else {
+    console.warn(
+      `%c❌ This is incorrect behavior. The service should return http://localhost:8000, instead it returned ${json.origin}.`,
+      'font-family: sans-serif;'
+    );
+  }
 })();
